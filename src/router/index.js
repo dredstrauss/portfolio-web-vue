5dd453dd46affaba5011bfa6a50e3a1d5dec0ad0
Dred Strauss <dred@dredstrauss.com>
2021-11-24 16:16:34 -0300

    Blog section page added

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 import Projects from '../views/Projects.vue'
+import Blog from '../views/Blog.vue'
 
 const routes = [
   {
@@ -18,6 +19,11 @@ const routes = [
     path: '/projects',
     name: 'Projects',
     component: Projects
+  },
+  {
+    path: '/blog',
+    name: 'Blog',
+    component: Blog
   }
 ]
 
---
+++ src/views/Blog.vue
@@ -0,0 +1,32 @@
+<template>
+
+  <Hero :title="texts.blog.hero.title" :subtitle="texts.blog.hero.subtitle" />
+
+</template>
+
+<script>
+import { useStore } from 'vuex'
+
+import Hero from '../components/Hero.vue'
+import SpinningWheel from '../components/SpinningWheel.vue'
+
+export default {
+  name: 'Blog',
+  components: {
+    Hero,
+    SpinningWheel
+  },
+  setup() {
+    const store = useStore();
+    const texts = store.state.texts;
+
+    return {
+      texts
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
