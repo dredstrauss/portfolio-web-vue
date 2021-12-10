e1a91aa35823636e5fcc3f48abfbd478a63114ae
Dred Strauss <dred@dredstrauss.com>
2021-12-10 12:03:17 -0300

    apiurl env var fixed

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore } from 'vuex'
 import { router } from '../router'
 
-const apiURL = process.env.apiURL;
+const apiURL = process.env.APIURL || 'https://dred-strauss-portfolio.herokuapp.com';
 
 export default createStore({
   state: {
